2801a74900ee84a53b39589a9d4d9306d47fabe1
Jade Li <jl@segsolutions.net>
2026-06-12 19:15:52 +0200

    initial file reader with only file reader functionality

---
+++ ios-pdfReader/ios-pdfReader/ios_pdfReaderApp.swift
@@ -0,0 +1,25 @@
+//
+/**
+ * © 2019 - 2025 SEG Solutions
+ *
+ * NOTICE: All information contained herein is, and remains
+ * the property of SEG Solutions and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to SEG Solutions and its suppliers.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from SEG Solutions.
+ */
+ 
+
+
+import SwiftUI
+
+@main
+struct ios_pdfReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
